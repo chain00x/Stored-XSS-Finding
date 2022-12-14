8b2f98f5170c427aa54cd063372eef6ba1f9ce3e
chain00x <90015694+chain00x@users.noreply.github.com>
2022-12-14 20:56:31 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
+chrome.tabs.onUpdated.addListener(function(tabId, props) {
+  if (props.status == "complete") {
+  // for the current tab, inject the "inject.js" file & execute it
+  chrome.tabs.executeScript(
+    tabId, 
+    {file:"inject.js"}
+  
+  )};
+});
+
 var enable=false;
 chrome.browserAction.onClicked.addListener(function (tab) {
  enable = enable ? false : true;
@@ -5,7 +15,7 @@ chrome.browserAction.onClicked.addListener(function (tab) {
   //turn on...
   chrome.browserAction.setIcon({ path: 'default.png' });
   chrome.browserAction.setBadgeText({ text: 'ON' });
-  chrome.tabs.executeScript(null, { file: 'inject.js' }); 
+  chrome.tabs.executeScript(null, { code: 'location.reload();' }); 
  }else{
   //turn off...
   chrome.browserAction.setIcon({ path: '1.png'});
